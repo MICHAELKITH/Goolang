150464ebb641c3eea07b36bff3d5fbdf2c14299e
MICHAELKITH <michaelkithinji27@gmail.com>
2024-03-08 22:57:17 +0300

    Advanced sort methods

--- main.go
+++ main.go
@@ -6,25 +6,13 @@ import (
 
 func main(){
 
-	// variable
-	// var nameOne string = "mike"//main.go
-
-	// nameOne := "mikee"
-
-	// var nameTwo = "Mike"
-
-	// fmt.Println(nameOne, nameTwo)
-
-
 	ages := []int{20, 49, 30, 38, 89, 43}
 
 	sort.Ints(ages)
 
 	fmt.Println(ages)
-index := sort.SearchInts(ages, 20)
+index := sort.SearchInts(ages, 89)
 fmt.Println(index)
-
-
 
 }
 
