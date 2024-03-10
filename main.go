dc382a97407f184d50b902cbc696e525f3b0858c
MICHAELKITH <michaelkithinji27@gmail.com>
2024-03-10 23:06:17 +0300

    Add sort golang

--- main.go
+++ main.go
@@ -6,12 +6,12 @@ import (
 
 func main(){
 
-	ages := []int{20, 49, 30, 38, 89, 43}
+	ages := []int{20, 49, 30, 38, 89, 43, 34 , 36 , 68 }
 
 	sort.Ints(ages)
 
 	fmt.Println(ages)
-index := sort.SearchInts(ages,20)
+index := sort.SearchInts(ages, 36)
 fmt.Println(index)
 
 }
